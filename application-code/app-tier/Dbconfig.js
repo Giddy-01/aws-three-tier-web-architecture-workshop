a7b752bd2ce9642da5417f4dadee36301f67db3c
Giddy-01 <gideonolatunbosun0@gmail.com>
2024-09-11 04:29:52 +0100

    Create Dbconfig.js

---
+++ application-code/app-tier/Dbconfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.c50ousme44i2.us-east-1.rds.amazonaws.com',
+    DB_USER : ‘admin’,
+    DB_PWD : ‘Olatunbosun1’,
+    DB_DATABASE : ‘webappdb’
+});
